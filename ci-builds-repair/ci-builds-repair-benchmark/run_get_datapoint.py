17b98cc45bae6fe62c5f044714049d44334ceab0
Juan Michelini <juan@juan.com.uy>
2025-03-26 21:48:20 +0100

    python scripts to initialize and complete instances/datapoints

---
+++ ci-builds-repair/ci-builds-repair-benchmark/run_get_datapoint.py
@@ -0,0 +1,30 @@
+import json
+import yaml
+import click
+from benchmark_functions import get_datapoint
+
+def load_config():
+    with open("config.yaml", "r") as file:
+        return yaml.safe_load(file)
+
+@click.command()
+@click.argument('json_input', type=click.STRING)
+def process_json(json_input):
+    try:
+        datapoint = json.loads(json_input)
+    except json.JSONDecodeError:
+        click.echo("Invalid JSON input", err=True)
+        return
+    
+    config = load_config()
+    
+    self.credentials = {
+        "username": self.config.username_gh,
+        "token": os.environ.get("TOKEN_GH"),
+        "model": model_name,
+    }
+    result = get_datapoint(datapoint, config, credentials)
+    click.echo(json.dumps(result, indent=2))
+
+if __name__ == "__main__":
+    process_json()
---
+++ ci-builds-repair/ci-builds-repair-benchmark/run_push_datapoint.py
@@ -0,0 +1,28 @@
+import json
+import yaml
+import click
+import git
+from benchmark_functions import push_repo
+
+def load_config():
+    with open("config.yaml", "r") as file:
+        return yaml.safe_load(file)
+
+@click.command()
+@click.argument('repo_path', type=click.STRING)
+def process_json(json_input):
+    config = load_config()
+    repo = git.Repo(repo_path)
+    self.credentials = {
+        "username": self.config.username_gh,
+        "token": os.environ.get("TOKEN_GH"),
+        "model": model_name,
+    }
+    benchmark_owner = config.benchmark_owner
+    user_branch_name = config.user_branch_name
+    result = push_repo(repo, credentials, benchmark_owner, user_branch_name)
+    click.echo(json.dumps(result, indent=2))
+
+if __name__ == "__main__":
+    process_json()
+
