0c339750cf42d872a17dbe17cc61bbdca1ef18a1
Juan Michelini <juan@juan.com.uy>
2025-04-02 15:30:51 +0200

    Better output of results

--- ci-builds-repair/ci-builds-repair-benchmark/run_push_datapoint.py
+++ ci-builds-repair/ci-builds-repair-benchmark/run_push_datapoint.py
@@ -18,7 +18,13 @@ def load_config():
 @click.option('--user_branch_name', type=str, required=True, help="user branch name provided by run_get_datapoint")
 @click.option('--repo_name', type=str, required=True, help="repo name in instace datapoint")
 @click.option('--repo_owner', type=str, required=True, help="repo owner in instance datapoint")
-def process_json(repo_path, model_name, user_branch_name, repo_name, repo_owner ):
+@click.option('--json_input', type=str, required=True, help="JSON string input")
+def process_json(repo_path, model_name, user_branch_name, repo_name, repo_owner, json_input ):
+    try:
+        datapoint = json.loads(json_input)
+    except json.JSONDecodeError:
+        click.echo("Invalid JSON input", err=True)
+        return "Invalid JSON"
     try:
         config = SimpleNamespace(**load_config())
         config.user_branch_name = user_branch_name
@@ -31,7 +37,15 @@ def process_json(repo_path, model_name, user_branch_name, repo_name, repo_owner 
         }
         benchmark_owner = config.benchmark_owner
         user_branch_name = config.user_branch_name
-        result = push_repo(repo, credentials, benchmark_owner, user_branch_name)
+        commit_sha  = push_repo(repo, credentials, benchmark_owner, user_branch_name)
+        job_identificator = {
+            "repo_name": repo.name,
+            "commit": commit_sha,
+            "id": datapoint["id"],
+            "sha_original": datapoint["sha_fail"],
+            "branch_name": user_branch_name,
+            "difficulty": datapoint["difficulty"],
+        }
         click.echo(result)
         return 0
     except Exception as e:
