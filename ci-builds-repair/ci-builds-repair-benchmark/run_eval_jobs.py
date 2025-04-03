ac67b6c0631666ef33743800a4ef27a836430d34
Juan Michelini <juan@juan.com.uy>
2025-04-03 20:42:32 +0200

    added eval script

---
+++ ci-builds-repair/ci-builds-repair-benchmark/run_eval_jobs.py
@@ -0,0 +1,17 @@
+import click
+from benchmark import CIFixBenchmark
+from benhmark_functions import fix_apply_diff, fix_none
+
+@click.command()
+@click.option('--model_name', required=True, help='Name of the model')
+@click.option('--config_path', required=True, help='Path to the configuration file')
+@click.option('--result_filename', required=True, help='Filename to store the results')
+@click.option('--job_ids_file', required=True, help='Path to the job IDs file')
+def run_benchmark(model_name, config_path, result_filename, job_ids_file):
+    """Run the CIFixBenchmark with the specified parameters."""
+    CIBenchPython = CIFixBenchmark(model_name, config_path)
+    job_results = CIBenchPython.eval_jobs(job_ids_file=job_ids_file, result_filename=result_filename)
+    click.echo(f"Benchmark completed. Results saved to {result_filename}")
+
+if __name__ == '__main__':
+    run_benchmark()
